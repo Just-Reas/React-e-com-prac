b746696909922a6e474669000eecb1687d38f094
Reas <145970644+Just-Reas@users.noreply.github.com>
2025-11-18 14:59:04 +0300

    Started third block

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import FirstSection from "./components/first-section/first-section";
 import Header from "./components/header/header";
+import SecondSection from "./components/second-section/second-section";
 
 function App() {
   return (
     <>
       <header>
-        <Header/>
+        <Header />
       </header>
       <main>
         <FirstSection />
+        <SecondSection />
       </main>
     </>
   );
