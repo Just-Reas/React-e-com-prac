c3440b906a530eb6561172ef264a016cbcad7654
Reas <145970644+Just-Reas@users.noreply.github.com>
2025-12-06 03:20:30 +0300

    Updated
    Finished signIn/signout window
    Started signIn window

--- src/components/log-page/log.jsx
+++ src/components/log-page/log.jsx
@@ -1,9 +1,12 @@
 import styles from "./log.module.scss";
 
 const LogSection = () => {
-    const consoleLog = () =>{
-        console.log(`AAAAAAAAAAA`)
-    }
+  const toSignIn = () => {
+    const sign = document.getElementById("sign-container");
+    const signIn = document.getElementById("signin-container");
+    sign.style.display = "none";
+    signIn.style.display = "flex";
+  };
   return (
     <div className={styles.logInner}>
       <div className="containerSup">
@@ -15,12 +18,31 @@ const LogSection = () => {
               alt="first-img-cutted"
             />
           </div>
-          <div className={styles.logButtonsInner}>
+          <div className={styles.container} id="sign-container">
             <div className={styles.logButtonsContainer}>
               <div className={styles.buttonText}>Sign In/Up</div>
               <div className={styles.buttonLine}></div>
-              <button onClick={consoleLog} className={styles.buttonLogin}>SIGN IN</button>
-              <button href="#" className={styles.buttonRegister}>SIGN UP</button>
+              <button onClick={toSignIn} className={styles.buttonLogin}>
+                SIGN IN
+              </button>
+              <button href="#" className={styles.buttonRegister}>
+                SIGN UP
+              </button>
+            </div>
+          </div>
+          <div className={styles.containerLogin} id="signin-container">
+            <div className={styles.containerLoginInner}>
+              <div className={styles.loginText}>Sign In</div>
+              <div className={styles.buttonLine}></div>
+              <div className={styles.emailContainer}>
+                <label className={styles.label} htmlFor="login-email">Email</label>
+                <input className={styles.input} type="email" required id="login-email"/>
+              </div>
+              <div className={styles.passwordContainer}>
+                <label htmlFor="login-password">Password</label>
+                <input type="password" id="login-password" />
+              </div>
+              <button className={styles.loginButton}>Go</button>
             </div>
           </div>
         </div>
